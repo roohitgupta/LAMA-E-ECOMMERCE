e4b8c4d0e58b89c4cbb2ba3f78fb567d6c152b5d
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-05-08 03:24:29 +0530

    complete sliderbar with sliding animation

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://www.kindpng.com/picc/m/741-7411250_thumb-image-girl-with-shopping-bag-png-transparent.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://thumbs.dreamstime.com/b/happy-shopping-woman-credit-portrait-young-beautiful-smiling-card-many-colorful-bags-isolated-white-background-39625569.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
